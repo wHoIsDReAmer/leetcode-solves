5abb553918b8a404022556ccaaedc83d6c1543a3
wHoIsDReAmer <neverprogrammer@gmail.com>
2026-06-16 21:12:36 +0900

    feat: update 167 solutions

--- 167.two-sum-ii-input-array-is-sorted.rs
+++ 167.two-sum-ii-input-array-is-sorted.rs
@@ -1,5 +1,20 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut m: HashMap<i32, usize> = HashMap::new();
 
+        for (i, n) in numbers.iter().enumerate() {
+            let c = target - n;
+
+            if let Some(&i2) = m.get(&c) {
+                return vec![(i2+1) as i32, (i+1) as i32]
+            }
+
+            m.insert(*n, i);
+        }
+
+        // unreachable
+        vec![]
     }
 }
